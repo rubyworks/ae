24390178eafdb3e64757ca7c6e43c212ba5922bd
7rans <transfire@gmail.com>
2010-06-06 10:54:08 -0400

    fixed embedded version number

--- lib/ae.rb
+++ lib/ae.rb
@@ -1,8 +1,9 @@
 module AE
-  VERSION="1.0.0"  # :till: VERSION="<%= version %>"
+  vers = YAML.load(File.read(File.dirname(__FILE__) + '/ae/version.yml'))
+  VERSION = vers.values_at('major', 'minor', 'patch', 'state', 'build').compact.join('.')
 end
 
 require 'ae/assert'
 require 'ae/expect'
 
-# Copyright (c) 2008,2009 Thomas Sawyer
+# Copyright (c) 2008 Thomas Sawyer

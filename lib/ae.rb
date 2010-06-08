9e5a4a229742ef334afa31c07d24509bec88b0e9
7rans <transfire@gmail.com>
2010-06-07 22:15:54 -0400

    require 'yaml' in ae.rb

--- lib/ae.rb
+++ lib/ae.rb
@@ -1,3 +1,5 @@
+require 'yaml'
+
 module AE
   vers = YAML.load(File.read(File.dirname(__FILE__) + '/ae/version.yml'))
   VERSION = vers.values_at('major', 'minor', 'patch', 'state', 'build').compact.join('.')

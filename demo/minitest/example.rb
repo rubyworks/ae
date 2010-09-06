3aff3a4c6f13987ae9a037d24d5a933cdf21dc55
7rans <transfire@gmail.com>
2010-09-06 09:41:59 -0400

    test: update tests to confrom to changes

---
+++ demo/minitest/example.rb
@@ -0,0 +1,34 @@
+require 'minitest/autorun'
+require 'ae/adapters/minitest'
+
+class MiniTestSupport < MiniTest::Unit::TestCase
+
+  def test_assert_pass
+    x = 5
+    y = 5
+    x.assert == y
+  end
+
+  def test_assert_fail
+    x = 5
+    y = 6
+    x.assert == y
+  end
+
+  def test_assert_fail_original
+    x = 5
+    y = 6
+    assert_equal(x, y)
+  end
+
+  def test_assert_pass_original
+    x = 5
+    y = 5
+    assert_equal(x, y)
+  end
+
+  def test_exception
+    raise
+  end
+
+end

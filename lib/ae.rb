87b850af972145d25a12334658175cf807653df8
7rans <transfire@gmail.com>
2010-12-15 14:40:54 -0500

    adjest who project metadata is loaded into ae at runtime

--- lib/ae.rb
+++ lib/ae.rb
@@ -1,6 +1,22 @@
 # AE, Copyright (c) 2008 Thomas Sawyer
+module AE
+  #
+  def self.metadata
+    @metadata ||= (
+      require 'yaml'
+      YAML.load(File.new(File.dirname(__FILE__) + '/ae.yml'))
+    )
+  end
 
-require 'ae/version'
+  #
+  def self.const_missing(name)
+    key = name.to_s.downcase
+    metadata[key] || super(name)
+  end
+
+  # becuase Ruby 1.8~ gets in the way :(
+  VERSION = metadata['version']
+end
+
 require 'ae/assert'
 require 'ae/expect'
-

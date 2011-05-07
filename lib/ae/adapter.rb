4f17f4d6161dab5b402f7c339151386ecf2fb273
7rans <transfire@gmail.com>
2011-05-07 10:09:52 -0400

    auto-adapter minitest needs to be checked first and use new RSpec namespace

--- lib/ae/adapter.rb
+++ lib/ae/adapter.rb
@@ -1,7 +1,8 @@
-if defined?(::Test::Unit)
-  require 'ae/adapters/testunit'
-elsif defined?(::MiniTest)
+if defined?(::MiniTest)
   require 'ae/adapters/minitest'
-elsif defined?(::Spec)
+elsif defined?(::Test::Unit)
+  require 'ae/adapters/testunit'
+elsif defined?(::RSpec)
   require 'ae/adapters/rspec'
 end
+

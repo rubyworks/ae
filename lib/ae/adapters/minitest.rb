bc3ced5a27774cda20ecb29ef355a36786b20b53
7rans <transfire@gmail.com>
2011-11-03 20:27:25 -0400

    Update minitest adapter to return on skip.

--- lib/ae/adapters/minitest.rb
+++ lib/ae/adapters/minitest.rb
@@ -4,6 +4,7 @@ AE.assertion_error = ::MiniTest::Assertion
 
 module MiniTest #:nodoc:
   class Unit #:nodoc:
+
     # MiniTest tracks assertion counts internally in it's Unit class via the 
     # +assertion_count+ attribute. To work with AE we need add in AE's assertion
     # total by overriding the +assertion_count+ method.
@@ -12,29 +13,34 @@ module MiniTest #:nodoc:
     def assertion_count
       @assertion_count + AE::Assertor.counts[:total]
     end
+
     # To teach MiniTest to recognize AE's expanded concept of assertions
     # we add in an extra capture clause to it's #puke method.
     #
     # @return [String] Status code is `S`, `F`, or `E`.
-    def puke c, m, x
-      case x
+    def puke k, m, e
+      case e
       when MiniTest::Skip
-        @skips = @skips + 1
-        x = "Skipped:\n#{m}(#{c}) [#{location x}]:\n#{x.message}\n"
+        @skips += 1
+        return "S" unless @verbose
+        e = "Skipped:\n#{m}(#{k}) [#{location e}]:\n#{e.message}\n"
       when MiniTest::Assertion
-        @failures = @failures + 1
-        x = "Failure:\n#{m}(#{c}) [#{location x}]:\n#{x.message}\n"
-      when x.respond_to?(:assertion?) && x.assertion?
-        @failures = @failures + 1
-        x = "Failure:\n#{m}(#{c}) [#{location x}]:\n#{x.message}\n"
+        @failures += 1
+        e = "Failure:\n#{m}(#{k}) [#{location e}]:\n#{e.message}\n"
       else
-        @errors = @errors + 1
-        b = MiniTest::filter_backtrace(x.backtrace).join("\n ")
-        x = "Error:\n#{m}(#{c}):\n#{x.class}: #{x.message}\n #{b}\n"
+        if e.respond_to?(:assertion?) && e.assertion?
+          @failures += 1
+          e = "Failure:\n#{m}(#{c}) [#{location e}]:\n#{e.message}\n"
+        else
+          @errors += 1
+          b = MiniTest::filter_backtrace(e.backtrace).join "\n    "
+          e = "Error:\n#{m}(#{k}):\n#{e.class}: #{e.message}\n    #{b}\n"
+        end
       end
-      @report << x
-      x[0, 1]
+      @report << e
+      e[0, 1]
     end
+
   end
 end
 
